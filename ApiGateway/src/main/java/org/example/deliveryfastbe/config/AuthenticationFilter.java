f225d7e3615a58bbb1aa37e6542a51f703d57db6
KietLe261003 <letuankiet26102003kl@gmail.com>
2025-02-21 21:42:49 +0700

    test

--- ApiGateway/src/main/java/org/example/deliveryfastbe/config/AuthenticationFilter.java
+++ ApiGateway/src/main/java/org/example/deliveryfastbe/config/AuthenticationFilter.java
@@ -3,6 +3,7 @@ package org.example.deliveryfastbe.config;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.ws.rs.core.HttpHeaders;
+import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
 import org.example.deliveryfastbe.Dto.Response.ApiResponse;
 import org.example.deliveryfastbe.Service.IdentityService;
@@ -12,12 +13,14 @@ import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -27,8 +30,24 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
     IdentityService identityService;
     @Autowired
     ObjectMapper objectMapper;
+
+    @NonFinal
+    String[] publicEnpoint= {
+            "/api/userservice/auth",
+            "/api/userservice/auth/checktoken"
+    };
+
+
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        if(isPublicEnpoint(exchange.getRequest()))
+        {
+            return chain.filter(exchange);
+        }
+
+        //Token
         List<String> authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION);
         if (CollectionUtils.isEmpty(authHeader))
             return unauthenticated(exchange.getResponse());
@@ -45,6 +64,9 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
                 })
                 .onErrorResume(throwable -> failConnectUserService(exchange.getResponse()));
 
+    }
+    private boolean isPublicEnpoint(org.springframework.http.server.reactive.ServerHttpRequest request) {
+        return Arrays.stream(publicEnpoint).anyMatch(s -> request.getURI().getPath().matches(s));
     }
 
     @Override
